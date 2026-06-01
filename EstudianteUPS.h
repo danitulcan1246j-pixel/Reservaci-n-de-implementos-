f7042126e843939209dc30b3b58045ad3819f62a
danitulcan1246j-pixel <danitulcan1246j@gmail.com>
2026-05-31 22:58:00 -0500

    Create EstudianteUPS.h

---
+++ EstudianteUPS.h
@@ -0,0 +1,16 @@
+#ifndef EstudianteUPS.h
+#define EstudianteUPS.h
+#include <string>
+
+class EstudianteUPS {
+public:
+    int idEstudiante;
+    std::string nombre;
+    std::string cedula;
+    std::string email;
+    int edad;
+    std::string ciudad;
+    std::string telefono;
+    std::string foto;
+};
+#endif
