8e143f63919b51cafefdffe97b02c45d5622bfb0
danitulcan1246j-pixel <danitulcan1246j@gmail.com>
2026-05-31 23:22:37 -0500

    Update JuegoMesa.cpp

--- JuegoMesa.cpp
+++ JuegoMesa.cpp
@@ -1,8 +1,12 @@
 #include "JuegoMesa.h"
-#include <iostream>
 
-int JuegoMesa::contarPiezas() { return cantPiezas; }
-bool JuegoMesa::validarPiezas() { return completo; }
-void JuegoMesa::reportarFaltante() { std::cout << "Reportando faltante..." << std::endl; }
-void JuegoMesa::ingresar() { std::cout << "Juego ingresado." << std::endl; }
-std::string JuegoMesa::obtenerInfo() { return "Juego: " + nombre; }
+JuegoMesa::JuegoMesa(int id, string n, int piezas, string edit) : Articulo() {
+    this->setIdArticulo(id);
+    this->setNombre(n);
+    this->cantPiezas = piezas;
+    this->editorial = edit;
+}
+
+string JuegoMesa::obtenerInfo() {
+    return "Juego de Mesa: " + this->getNombre() + " | Editorial: " + editorial;
+}
