5751c0381e89c6d51c46263df1901d551c0834f0
danitulcan1246j-pixel <danitulcan1246j@gmail.com>
2026-05-31 23:10:19 -0500

    Create Prenda.cpp

---
+++ Prenda.cpp
@@ -0,0 +1,6 @@
+#include "Prenda.h"
+#include <iostream>
+
+void Prenda::ingresar() { std::cout << "Prenda ingresada." << std::endl; }
+void Prenda::registrarRecibo() { std::cout << "Recibo registrado." << std::endl; }
+bool Prenda::validarEstado() { return true; }
