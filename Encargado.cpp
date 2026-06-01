f146f1e13aaa7429de885c2953129473a4db5286
danitulcan1246j-pixel <danitulcan1246j@gmail.com>
2026-05-31 23:10:40 -0500

    Create Encargado.cpp

---
+++ Encargado.cpp
@@ -0,0 +1,5 @@
+#include "Encargado.h"
+#include <iostream>
+
+bool Encargado::validarEntrega() { return true; }
+void Encargado::autorizarPrestamo() { std::cout << "Prestamo autorizado." << std::endl; }
