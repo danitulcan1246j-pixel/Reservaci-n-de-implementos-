cd5cf48a0fea14603cff2855f12cae72763f26ff
danitulcan1246j-pixel <danitulcan1246j@gmail.com>
2026-05-31 23:20:16 -0500

    Update Articulo.h

--- Articulo.h
+++ Articulo.h
@@ -1,17 +1,40 @@
 #ifndef Articulo.h
 #define Articulo.h
 #include <string>
+#include <iostream>
 #include "CategoriaArticulo.h"
 
+using namespace std;
+
 class Articulo {
-protected:
+private:
     int idArticulo;
-    std::string nombre;
-    std::string estado;
+    string nombre;
+    string estado;
+    string descripcion;
     bool disponible;
+
 public:
     CategoriaArticulo categoria;
-    virtual ~Articulo() {}
-    virtual bool verificarDisp() = 0;
+
+    Articulo();
+    Articulo(int _id, string _nombre, string _estado, string _desc, bool _disponible);
+    virtual ~Articulo();
+
+    void setIdArticulo(int _id);
+    int getIdArticulo() const;
+    void setNombre(string _nombre);
+    string getNombre() const;
+    void setEstado(string _estado);
+    string getEstado() const;
+    void setDescripcion(string _desc);
+    string getDescripcion() const;
+    void setDisponible(bool _disponible);
+    bool getDisponible() const;
+
+    virtual bool verificarDisp();
+    virtual void cambiarEstado(string _estado);
+    virtual string obtenerInfo();
+    virtual void imprimir() const;
 };
 #endif
