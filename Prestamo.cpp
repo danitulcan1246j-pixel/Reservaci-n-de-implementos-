fdd8ed95c83f062547acc2001ad81432824844f9
danitulcan1246j-pixel <danitulcan1246j@gmail.com>
2026-05-31 23:10:58 -0500

    Create Prestamo.cpp

---
+++ Prestamo.cpp
@@ -0,0 +1,6 @@
+#include "Prestamo.h"
+#include <iostream>
+
+void Prestamo::registrar() { std::cout << "Prestamo registrado." << std::endl; }
+int Prestamo::calcularDiasPrestamo() { return 0; } // Lógica pendiente
+void Prestamo::imprimir(std::string query) { std::cout << "Imprimiendo: " << query << std::endl; }
